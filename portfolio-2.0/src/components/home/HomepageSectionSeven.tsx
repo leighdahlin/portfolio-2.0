fd5efd5876dcded98d337514e8ffcac497f80556
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-24 15:34:35 +0100

    attempt to fix tailwind css by adding custom style selector

--- portfolio-2.0/src/components/home/HomepageSectionSeven.tsx
+++ portfolio-2.0/src/components/home/HomepageSectionSeven.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Image from 'next/image';
 import { shrikhand } from '@/app/fonts';
-import './custom.css';
+import '../../app/globals.css';
 
 export default function HomepageSectionSeven() {
   return (
