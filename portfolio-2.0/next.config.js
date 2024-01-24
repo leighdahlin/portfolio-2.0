52212cf084c367b89b68de4930dc054091cb7d39
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-24 14:56:39 +0100

    udpate next configuration for images

--- portfolio-2.0/next.config.js
+++ portfolio-2.0/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.main--extraordinary-cranachan-fe7e16.netlify.app',
+        port: '',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
