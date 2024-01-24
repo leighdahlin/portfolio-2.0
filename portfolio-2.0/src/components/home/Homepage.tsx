9fa29829efd71cf9e41ba49b89e2a1833731837a
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-24 09:16:53 +0100

    comment out unused imports

--- portfolio-2.0/src/components/home/Homepage.tsx
+++ portfolio-2.0/src/components/home/Homepage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import HomepageSectionOne from './HomepageSectionOne';
 import HomepageSectionTwo from './HomepageSectionTwo';
-import HomepageSectionThree from './HomepageSectionThree';
-import HomepageSectionFour from './HomepageSectionFour';
+// import HomepageSectionThree from './HomepageSectionThree';
+// import HomepageSectionFour from './HomepageSectionFour';
 import HomepageSectionFive from './HomepageSectionFive';
 import HomepageSectionSix from './HomepageSectionSix';
 import HomepageSectionSeven from './HomepageSectionSeven';
--- portfolio-2.0/src/components/home/HomepageSectionFour.tsx
+++ portfolio-2.0/src/components/home/HomepageSectionFour.tsx
@@ -1,6 +1,6 @@
 'use client';
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import roadMapBackground from './img/road-map.png';
 import { shrikhand } from '@/app/fonts';
