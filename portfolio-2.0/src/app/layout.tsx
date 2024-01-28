777fb8471b090362125cb52dd8d35a38f8955ef5
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-28 13:52:47 +0100

    add open graph meta tags for link previews

--- portfolio-2.0/src/app/layout.tsx
+++ portfolio-2.0/src/app/layout.tsx
@@ -2,13 +2,8 @@
 
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-// import type { Metadata } from 'next';
+import { Helmet } from 'react-helmet';
 import './globals.css';
-
-// export const metadata: Metadata = {
-//   title: 'Leigh Dahlin',
-//   description: 'Website Freelancer',
-// };
 
 export default function RootLayout({
   children,
@@ -17,6 +12,34 @@ export default function RootLayout({
 }) {
   return (
     <HelmetProvider>
+      <Helmet>
+        <title>Leigh Dahlin - Freelance Website Developer and Designer</title>
+        <meta
+          name="description"
+          content="Leigh Dahlin is a freelance website developer or design that can assist you in creating or maintaining your website."
+        />
+        <meta
+          property="og:title"
+          content="Leigh Dahlin - Freelance Website Developer and Designer"
+        />
+        <meta
+          property="og:description"
+          content="Leigh Dahlin is a freelance website developer or design that can assist you in creating or maintaining your website."
+        />
+        <meta
+          property="og:image"
+          content="https://leighdahlin.com/images/link-preview.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://leighdahlin.com/" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="keywords"
+          content="Leigh Dahlin, Freelancer, Developer, Website Developer, Designer, Website Designer, Custom Website"
+        ></meta>
+      </Helmet>
       <html lang="en">
         <body>{children}</body>
       </html>
