d659c325dbd3ae156903fc23cfbddd954cc921b4
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-06-03 15:40:42 +0200

    fix build errors resulting from removing 'use client' from page.tsx

--- portfolio-2.0/src/app/page.tsx
+++ portfolio-2.0/src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import GlobalLayout from '@/components/GlobalLayout';
 import Home from '@/components/home/Homepage';
--- portfolio-2.0/src/components/home/AboutCard.tsx
+++ portfolio-2.0/src/components/home/AboutCard.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import useIsMobile from '@/hooks/useIsMobile';
 import { AboutCardInfoType } from './AboutBlocks';
