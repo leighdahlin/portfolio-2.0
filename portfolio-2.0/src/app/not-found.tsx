ec8d385601f70a350d67ed18c02a23eee70270a7
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-08-19 12:26:28 +0200

    update not found page

--- portfolio-2.0/src/app/not-found.tsx
+++ portfolio-2.0/src/app/not-found.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import GlobalLayout from '@/components/common/GlobalLayout';
+import PageNotFound from '@/components/not-found/PageNotFound';
 
 export default function NotFound() {
   return (
     <GlobalLayout>
-      <div className="bg-off-white dark:bg-soft-black flex flex-col gap-[2rem] items-center justify-center h-full">
-        <p className="text-black dark:text-off-white">Page Not Found</p>
-        <a
-          href="/"
-          className="p-[0.75rem] min-w-[175px] bg-golden text-off-white text-center text-sm md:text-base rounded-[.25rem] font-medium tracking-[.1rem] hover:bg-hover-golden"
-        >
-          Back to Home
-        </a>
-      </div>
+      <PageNotFound />
     </GlobalLayout>
   );
 }
