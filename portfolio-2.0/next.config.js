f30e272833c02859b60001948172b12e482a26a7
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-08-21 17:10:42 +0200

    setup for decap cms and enablereading of markdown files

--- portfolio-2.0/next.config.js
+++ portfolio-2.0/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['https://extraordinary-cranachan-fe7e16.netlify.app/*'],
   },
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
 };
 
 module.exports = nextConfig;
